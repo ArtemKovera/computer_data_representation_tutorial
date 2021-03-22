1874f1578c83b752a3440f53d53db06afcc2f552
Artem Kovera <artemkovera@gmail.com>
2021-03-22 13:14:50 +0200

    slight modifications

--- exploring_properties_of_floating_point_numbers.c
+++ exploring_properties_of_floating_point_numbers.c
@@ -58,12 +58,11 @@ int main()
     }
     
     printf("\n");
-    void * p3 = &var3;
-    void * p4 = &var4;
+
     printf("var3 in binary: ");
-    showBitsInFloat(p3);
+    showBitsInFloat(&var3);
     printf("var4 in binary: ");
-    showBitsInDouble(p4);
+    showBitsInDouble(&var4);
 
     /////////////////////////////////////////////
     printf("---------\n\n");
@@ -86,17 +85,16 @@ int main()
     }
 
     printf("\n");
-    void * p5 = &var5;
-    void * p6 = &var6;
+
     printf("va5 in binary: ");
-    showBitsInFloat(p5);
+    showBitsInFloat(&var5);
     printf("var6 in binary: ");
-    showBitsInDouble(p6);
+    showBitsInDouble(&var6);
 
     //////////////////////////////////////
     printf("---------\n\n");
 
-    float var7 = 0.5;
+    float var7  = 0.5;
     double var8 = 0.5;
     
     printf("double var8 = %f | float var7 = %f \n\n", var8, var7);
@@ -115,12 +113,11 @@ int main()
     }
 
     printf("\n");
-    void * p7 = &var7;
-    void * p8 = &var8;
+
     printf("var7 in binary: ");
-    showBitsInFloat(p7);
+    showBitsInFloat(&var7);
     printf("var8 in binary: ");
-    showBitsInDouble(p8);
+    showBitsInDouble(&var8);
 
     printf("--------------------------------------------------\n\n");
 
@@ -131,8 +128,7 @@ int main()
     double z = 0.33;
     double r1 = y * x + y * z;
     double r2 = y * (x + z);
-    void * pr1 = &r1;
-    void * pr2 = &r2;
+
      
     printf("r1 = y * x + y * z =  %f\n", r1);
     printf("r2 = y * (x + z)   =  %f\n\n", r2);
@@ -141,9 +137,9 @@ int main()
         printf("r1 and r2 are NOT equal\n\n");
     
     printf("r1 in binary:  ");
-    showBitsInDouble(pr1);
+    showBitsInDouble(&r1);
     printf("r2 in binary:  ");
-    showBitsInDouble(pr2);
+    showBitsInDouble(&r2);
 
     printf("--------------------------------------------------\n\n");
     
@@ -154,8 +150,7 @@ int main()
     double c = 0.3333;
     double res1 = (a + b) + c;
     double res2 = a + (b + c);
-    void * pres1 = &res1;
-    void * pres2 = &res2;
+
      
     printf("res1 = (a + b) + c  =  %f\n", res1);
     printf("res2 = a + (b + c)  =  %f\n\n", res2);
@@ -164,101 +159,85 @@ int main()
         printf("res1 and res2 are NOT equal\n\n");
     
     printf("res1 in binary:  ");
-    showBitsInDouble(pres1);
+    showBitsInDouble(&res1);
     printf("res2 in binary:  ");
-    showBitsInDouble(pres2);
+    showBitsInDouble(&res2);
 
     printf("--------------------------------------------------\n\n");
 
     printf("*****infinity in calculations******\n");
 
     double infin = INFINITY;
-    void * infP = &infin;
     printf("Positive infinity:                      ");
-    showBitsInDouble(infP);
+    showBitsInDouble(&infin);
 
     double dv1 = pow (10, 308);
     double dv2 = dv1;
-    void * dp1 = &dv1;
     printf ("dv1 = dv2 = 10^308 in binary(double):   ");
-    showBitsInDouble(dp1);
+    showBitsInDouble(&dv1);
 
     double dv3 = dv1 * dv2;                   //infinity due to overflow
-    void * dp3 = &dv3;
     printf("dv1 * dv2:                              ");
-    showBitsInDouble(dp3);
+    showBitsInDouble(&dv3);
 
     double dv4 = 1.0 / 0.0;                   //infinity due to division by zero
-    void * dp4 = &dv4;
     printf("division of a positive number by zero:  ");
-    showBitsInDouble(dp4);
+    showBitsInDouble(&dv4);
 
     double dv55 = 1.0 / INFINITY;
-    void * dp55 = &dv55;
     printf("division of 1.0 by infinity:            ");
-    showBitsInDouble(dp55);
+    showBitsInDouble(&dv55);
 
     double dv66 = INFINITY / INFINITY;
-    void * dp66 = &dv66;
     printf("Infinity over infinity:                 ");
-    showBitsInDouble(dp66);
+    showBitsInDouble(&dv66);
 
     double dv77 = INFINITY / pow(10, 300);
-    void * dp77 = &dv77;
     printf("Infinity divided by 10^300:             ");
-    showBitsInDouble(dp77);
+    showBitsInDouble(&dv77);
 
     double dv88 = INFINITY - INFINITY;
-    void * dp88 = &dv88;
     printf("Infinity - Infinity:                    ");
-    showBitsInDouble(dp88);
+    showBitsInDouble(&dv88);
 
     double dv99 = INFINITY * INFINITY;
-    void * dp99 = &dv99;
     printf("Infinity * Infinity:                    ");
-    showBitsInDouble(dp99);
+    showBitsInDouble(&dv99);
 
     printf("--------------------------------------------------\n\n");
 
     printf("******* NaN in calculations *********\n");
    
     double nan = NAN;
-    void * nanP = &nan;
     printf("NaN:                                   ");
-    showBitsInDouble(nanP);
+    showBitsInDouble(&nan);
     
     
     double dv5 = 0.0 / 0.0;                    //nan due to zero over zero
-    void * dp5 = &dv5;
     printf("zero over zero:                        "); 
-    showBitsInDouble(dp5);
+    showBitsInDouble(&dv5);
     
 
     double dv6 = 1.0 / NAN;
-    void * dp6 = &dv6;
     printf("1 divided by NaN:                      ");
-    showBitsInDouble(dp6);
+    showBitsInDouble(&dv6);
 
 
     double dv7 = 0.0 / NAN;
-    void * dp7 = &dv7;
     printf("0.0 divided by NaN:                    ");
-    showBitsInDouble(dp7);
+    showBitsInDouble(&dv7);
 
     double dv8 = 1.0 + NAN;
-    void * dp8 = &dv8;
     printf("1 + NaN:                               ");
-    showBitsInDouble(dp8); 
+    showBitsInDouble(&dv8); 
 
     double dv9 = NAN + INFINITY;
-    void * dp9 = &dv9;
     printf("NaN + Infinity:                        ");   
-    showBitsInDouble(dp9);
+    showBitsInDouble(&dv9);
 
     double dv10 = NAN * INFINITY;
-    void * dp10 = &dv10;
     printf("NaN * Infinity:                        ");   
-    showBitsInDouble(dp10);    
+    showBitsInDouble(&dv10);    
 
     return 0;
 }
